8c9d04e06a9a8a63998e78b9254e16cd4d75dcf4
Clark Melchert <melchertclark@gmail.com>
2025-04-15 20:02:56 -0400

    Ensure Lambda handler is always up-to-date in deployment package

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 rm -rf build/ package.zip
 mkdir build
 pip install -r requirements.txt -t build/
-cp -r src/* build/
+cp src/lambda_function.py build/lambda_function.py
 cd build
 zip -r ../package.zip .
 cd ..
